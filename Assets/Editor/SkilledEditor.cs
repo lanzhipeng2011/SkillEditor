a487f253a368523b6d15c857c751860dc0a5853e
lanzhipeng2011 <414628888@qq.com>
2020-09-04 16:51:01 +0800

    使创建人物逻辑动态读取文件夹显示pop

--- Assets/Editor/SkilledEditor.cs
+++ Assets/Editor/SkilledEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public class SkilledEditor : EditorWindow
 {
@@ -13,23 +14,55 @@ public class SkilledEditor : EditorWindow
     {
         if (Application.isPlaying)
         {
+            GetAllFiles(new DirectoryInfo(filePath));
             skillEditor = GetWindow<SkilledEditor>("SkillEditor");
             skillEditor.Show();
         }
         else {
             Debug.Log("使用技能编辑器需使Unity处于运行中......");
+           
         }
     }
 
     private static PlayerSkillEditor playerSkillEditor;
-    string[] roleName = new string[] { "Null", "Boy", "Bear", "CubeBoss" };
+    private static string[] roleName;// = new string[] { "Null", "Boy", "Bear", "CubeBoss" };
     int roleIndex = 0;
     Player player = null;
     string playerName = "Null";
+    private static string filePath = Application.dataPath + "/Resources/Role";
+    private static string fullName;
+
+    public static void GetAllFiles(DirectoryInfo directoryInfo)
+    {
+        DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+        FileInfo[] fileInfos = directoryInfo.GetFiles();
+        roleName = new string[fileInfos.Length];
+        //for (int i = 0; i < directoryInfos.Length; i++)
+        //{
+        //    Debug.Log("DirectoryInfo:" + directoryInfos[i].FullName);
+        //    GetAllFiles(directoryInfos[i]);
+        //}
+        for (int i = 0; i < fileInfos.Length; i++)
+        {
+            if (!fileInfos[i].FullName.Contains(".meta"))
+            {
+                //Debug.Log("fileInfos:" + fileInfos[i].FullName);
+                fullName = fileInfos[i].FullName;
+                int startIndex = fullName.LastIndexOf('\\') + 1;
+                int len = fullName.LastIndexOf('.') - fullName.LastIndexOf('\\') - 1;
+                roleName[i] = fullName.Substring(startIndex,len); ;
+            }
+        }
 
 
+
+    }
     private void OnGUI()
     {
+
+
+
+
         roleIndex = EditorGUILayout.Popup("选择模型", roleIndex, roleName);
         if (GUILayout.Button("创建角色"))
         {
